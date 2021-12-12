d5150c0f7275812b13dc7de09fc6442858b2d0ba
Anuj Vasil <anujvasil@Anujs-MacBook-Pro.local>
2021-12-12 11:10:31 -0800

    fixed y speed

--- facetracking.py
+++ facetracking.py
@@ -19,9 +19,40 @@ while True:
     img = getFrame(drone)
 
     img, info = findFace(img)
-    pErrorx, pErrory = trackFace(drone, info, 640, 480, pidx, pidy, pErrorx, pErrory)
+    pErrorx, pErrory = trackFace(drone, info, 360, 240, pidx, pidy, pErrorx, pErrory)
 
     cv2.imshow('Image', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) and 0xFF == ord('q'):
         drone.land()
         break
+
+
+# from utils import *
+# import cv2
+ 
+# w,h = 360,240
+# pid = [0.4,0.4,0]
+# pError = 0
+# startCounter = 0  # for no Flight 1   - for flight 0
+ 
+ 
+# myDrone = initializeTello()
+ 
+# while True:
+ 
+#     ## Flight
+#     if startCounter == 0:
+#         myDrone.takeoff()
+#         startCounter = 1
+ 
+#     ## Step 1
+#     img = telloGetFrame(myDrone,w,h)
+#     ## Step 2
+#     img, info = findFace(img)
+#     ## Step 3
+#     pError = trackFace(myDrone,info,w,pid,pError)
+#     #print(info[0][0])
+#     cv2.imshow('Image',img)
+#     if cv2.waitKey(1) and 0xFF == ord('q'):
+#         myDrone.land()
+#         break
--- utils.py
+++ utils.py
@@ -5,12 +5,17 @@ import numpy as np
 def initDrone():
     drone = Tello()
     drone.connect()
+    drone.for_back_velocity = 0
+    drone. left_right_velocity = 0
+    drone.up_down_velocity = 0
+    drone.yaw_velocity = 0
+    drone.speed = 0
     print(drone.get_battery)
     drone.streamoff()
     drone.streamon()
     return drone
 
-def getFrame(drone, w = 640, h = 480):
+def getFrame(drone, w = 360, h = 240):
     frame = drone.get_frame_read()
     frame= frame.frame
     img = cv2.resize(frame, (w, h))
@@ -53,7 +58,7 @@ def trackFace(drone, info, w, h, pidx, pidy, pErrorx, pErrory):
     print(speedy)
     if info[0][0] != 0 and info[0][1] != 0:
         drone.yaw_velocity = speedx
-        drone.up_down_velocity = speedy
+        drone.up_down_velocity = -speedy
     else:
         drone.for_back_velocity = 0
         drone.left_right_velocity = 0
@@ -68,3 +73,73 @@ def trackFace(drone, info, w, h, pidx, pidy, pErrorx, pErrory):
                                 drone.yaw_velocity)
     return [errorx, errory]
 
+
+# from djitellopy import Tello
+# import cv2
+# import numpy as np
+ 
+ 
+# def initializeTello():
+#     myDrone = Tello()
+#     myDrone.connect()
+#     myDrone.for_back_velocity = 0
+#     myDrone. left_right_velocity = 0
+#     myDrone.up_down_velocity = 0
+#     myDrone.yaw_velocity = 0
+#     myDrone.speed = 0
+#     print(myDrone.get_battery())
+#     myDrone.streamoff()
+#     myDrone.streamon()
+#     return myDrone
+ 
+# def telloGetFrame(myDrone, w= 360,h=240):
+#     myFrame = myDrone.get_frame_read()
+#     myFrame = myFrame.frame
+#     img = cv2.resize(myFrame,(w,h))
+#     return img
+ 
+# def findFace(img):
+#     faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#     imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#     faces = faceCascade.detectMultiScale(imgGray,1.1,6  )
+ 
+#     myFaceListC = []
+#     myFaceListArea = []
+ 
+#     for (x,y,w,h) in faces:
+#         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+#         cx = x + w//2
+#         cy = y + h//2
+#         area = w*h
+#         myFaceListArea.append(area)
+#         myFaceListC.append([cx,cy])
+ 
+#     if len(myFaceListArea) !=0:
+#         i = myFaceListArea.index(max(myFaceListArea))
+#         return img, [myFaceListC[i],myFaceListArea[i]]
+#     else:
+#         return img,[[0,0],0]
+ 
+# def trackFace(myDrone,info,w,pid,pError):
+ 
+#     ## PID
+#     error = info[0][0] - w//2
+#     speed = pid[0]*error + pid[1]*(error-pError)
+#     speed = int(np.clip(speed,-100,100))
+ 
+ 
+#     print(speed)
+#     if info[0][0] !=0:
+#         myDrone.yaw_velocity = speed
+#     else:
+#         myDrone.for_back_velocity = 0
+#         myDrone.left_right_velocity = 0
+#         myDrone.up_down_velocity = 0
+#         myDrone.yaw_velocity = 0
+#         error = 0
+#     if myDrone.send_rc_control:
+#         myDrone.send_rc_control(myDrone.left_right_velocity,
+#                                 myDrone.for_back_velocity,
+#                                 myDrone.up_down_velocity,
+#                                 myDrone.yaw_velocity)
+#     return error
